787f90e83319147898dc31df29987bb3f47013b3
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-01 13:43:34 +0300

    Changed some codes to fix a bug

--- apptab.js
+++ apptab.js
@@ -1,1 +1,15 @@
-const
+const btns = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click", function (e) {
+    console.log(e.target.dataset.id);
+    const id = e.target.dataset.id;
+    if(id) {
+        // remove active from other button
+        btns.forEach(function(btn){
+           btn.classList.remove("active");
+           e.target.classList.add("active");
+        });
+    }
+});
