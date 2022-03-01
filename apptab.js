3fa382f1df4a35403dfaa14e53427c28f2e69c4f
Njeri Muchiri <muchirinjeri3210@gmail.com>
2022-03-01 13:49:08 +0300

    added new functionality

--- apptab.js
+++ apptab.js
@@ -3,7 +3,7 @@ const about = document.querySelector(".about");
 const articles = document.querySelectorAll(".content");
 
 about.addEventListener("click", function (e) {
-    console.log(e.target.dataset.id);
+    // console.log(e.target.dataset.id);
     const id = e.target.dataset.id;
     if(id) {
         // remove active from other button
@@ -11,5 +11,11 @@ about.addEventListener("click", function (e) {
            btn.classList.remove("active");
            e.target.classList.add("active");
         });
+        // hide other articles
+        articles.forEach(function(article){
+            article.classList.remove('active')
+        })
+        const element = document.getElementById(id);
+        element.classList.add("active");
     }
 });
